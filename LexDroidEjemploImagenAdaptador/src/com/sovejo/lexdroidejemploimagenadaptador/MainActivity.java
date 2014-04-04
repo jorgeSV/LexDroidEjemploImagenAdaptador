fcd2097cdabccff912cff18b7ffca58ecb32d79f
jorgeSV <amk114@gmail.com>
2014-04-04 17:50:35 +0200

    Creado el ImageAdapter.

--- LexDroidEjemploImagenAdaptador/src/com/sovejo/lexdroidejemploimagenadaptador/MainActivity.java
+++ LexDroidEjemploImagenAdaptador/src/com/sovejo/lexdroidejemploimagenadaptador/MainActivity.java
@@ -1,16 +1,23 @@
 package com.sovejo.lexdroidejemploimagenadaptador;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.widget.Gallery;
 import android.app.Activity;
 
 public class MainActivity extends Activity 
 {
-
+	Gallery galeria01;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) 
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		
+		galeria01 = (Gallery) findViewById(R.id.galeria01);
+		//galeria01.setAdapter(new myAdapter(this));
 	}
 
 }
