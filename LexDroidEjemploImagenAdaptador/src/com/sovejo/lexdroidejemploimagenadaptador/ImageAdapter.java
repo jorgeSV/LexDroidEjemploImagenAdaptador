a49015f9b49b7b7001b180125a6a59cb9846b153
jorgeSV <amk114@gmail.com>
2014-04-07 12:24:16 +0200

    Ejercicio completado.

--- LexDroidEjemploImagenAdaptador/src/com/sovejo/lexdroidejemploimagenadaptador/ImageAdapter.java
+++ LexDroidEjemploImagenAdaptador/src/com/sovejo/lexdroidejemploimagenadaptador/ImageAdapter.java
@@ -1,40 +1,70 @@
 package com.sovejo.lexdroidejemploimagenadaptador;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Gallery;
+import android.widget.ImageView;
 
 public class ImageAdapter extends BaseAdapter
 {
 	private Context myContext;
 	
+	//Array con los IDs de las imagenes para poder manejarlas luego
+	private Integer[] mImageIDs = {
+			R.drawable.uno,
+			R.drawable.dos,
+			R.drawable.tres,
+			R.drawable.cuatro,
+			R.drawable.cinco
+	};
+	
+	int mGalleryItemBackground;
+	
+	//Constructor
+	public ImageAdapter(Context c)
+	{
+		myContext = c;
+		TypedArray attr = myContext.obtainStyledAttributes(R.styleable.galeriaEstilo);
+		mGalleryItemBackground = attr.getResourceId(R.styleable.galeriaEstilo_android_galleryItemBackground, 0);
+		attr.recycle();
+	}
+	
 	@Override
 	public int getCount() 
 	{
 		// TODO Auto-generated method stub
-		return 0;
+		return mImageIDs.length;
 	}
 
 	@Override
 	public Object getItem(int position) 
 	{
 		// TODO Auto-generated method stub
-		return null;
+		return position;
 	}
 
 	@Override
 	public long getItemId(int position) 
 	{
 		// TODO Auto-generated method stub
-		return 0;
+		return position;
 	}
 
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) 
 	{
 		// TODO Auto-generated method stub
-		return null;
+		ImageView imageView = new ImageView(myContext);
+		
+		imageView.setImageResource(mImageIDs[position]);
+		imageView.setLayoutParams(new Gallery.LayoutParams(200, 200));
+		imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+		imageView.setBackgroundResource(mGalleryItemBackground);
+		
+		return imageView;
 	}
 	
 }
--- LexDroidEjemploImagenAdaptador/src/com/sovejo/lexdroidejemploimagenadaptador/MainActivity.java
+++ LexDroidEjemploImagenAdaptador/src/com/sovejo/lexdroidejemploimagenadaptador/MainActivity.java
@@ -1,11 +1,15 @@
 package com.sovejo.lexdroidejemploimagenadaptador;
 
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.Gallery;
+import android.widget.Toast;
 import android.app.Activity;
 
-public class MainActivity extends Activity 
+public class MainActivity extends Activity
 {
 	Gallery galeria01;
 	
@@ -13,11 +17,18 @@ public class MainActivity extends Activity
 	protected void onCreate(Bundle savedInstanceState) 
 	{
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.activity_main);
 		
 		galeria01 = (Gallery) findViewById(R.id.galeria01);
-		//galeria01.setAdapter(new myAdapter(this));
+		galeria01.setAdapter(new ImageAdapter(this));
+		galeria01.setOnItemClickListener(new OnItemClickListener() 
+		{
+			public void onItemClick(AdapterView<?> parent, View v, int position, long id)
+			{
+				Toast.makeText(MainActivity.this, ""+position, Toast.LENGTH_LONG).show();
+			}
+		});
 	}
 
 }
